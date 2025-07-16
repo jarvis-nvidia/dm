22034f585dc5d77ef8fd43444faeedf01c4f0846
KARTIK GARG <125589036+Kartikgarg74@users.noreply.github.com>
2025-07-16 15:22:02 +0530

    Added build-manifest.json

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/QNIiGjinyXZ4AzVlFvERqj8RTTHaR+O+PiBs6pn7RE=\"\n}"
